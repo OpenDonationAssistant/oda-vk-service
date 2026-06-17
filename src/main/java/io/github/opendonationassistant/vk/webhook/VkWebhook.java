eec7313740faf2499e1fc96963fb3026e3ac1cae
stCarolas <stcarolas@gmail.com>
2026-06-17 13:52:17 +0300

    fix receiving event

--- src/main/java/io/github/opendonationassistant/vk/webhook/VkWebhook.java
+++ src/main/java/io/github/opendonationassistant/vk/webhook/VkWebhook.java
@@ -2,7 +2,6 @@ package io.github.opendonationassistant.vk.webhook;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.micronaut.http.MediaType;
-import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Consumes;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
@@ -10,6 +9,7 @@ import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
+import io.micronaut.serde.ObjectMapper;
 import io.micronaut.serde.annotation.Serdeable;
 import jakarta.inject.Inject;
 import java.util.List;
@@ -22,22 +22,29 @@ public class VkWebhook {
 
   private final Logger log = LoggerFactory.getLogger(VkWebhook.class);
   private final List<VkEventHandler> handlers;
+  private final ObjectMapper mapper;
 
   @Inject
-  public VkWebhook(List<VkEventHandler> handlers) {
+  public VkWebhook(List<VkEventHandler> handlers, ObjectMapper mapper) {
     this.handlers = handlers;
+    this.mapper = mapper;
   }
 
   @Post("/vklive/events")
   @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
   @Secured(SecurityRule.IS_ANONYMOUS)
   @ExecuteOn(TaskExecutors.BLOCKING)
-  public void listenVklive(@Body Event event) {
-    log.info("Received event: {}", event);
-    handlers
-      .stream()
-      .filter(it -> it.canHandle(event.type()))
-      .forEach(it -> it.handle(event).join());
+  public void listenVklive(String message) {
+    log.info("Received event: {}", message);
+    try {
+      var event = mapper.readValue(message, Event.class);
+      handlers
+        .stream()
+        .filter(it -> it.canHandle(event.type()))
+        .forEach(it -> it.handle(event).join());
+    } catch (Exception e) {
+      log.error("Failed to handle event", e.getMessage());
+    }
   }
 
   @Serdeable
